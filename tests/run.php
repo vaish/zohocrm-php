04c58aa6be544e51752bf20a00114b289b55c640
Frangeris Peguero <frangeris.20@gmail.com>
2013-11-15 11:16:40 -0400

    Updating credentials

--- tests/run.php
+++ tests/run.php
@@ -6,6 +6,6 @@ require_once '/home/composer/public_html/zohocrm-php/vendor/autoload.php';
 use Zoho\CRM\Client;
 
 // Create the client
-$zoho = new Client('');
+$zoho = new Client('ACCESS_TOKEN');
 
 var_dump($zoho);
